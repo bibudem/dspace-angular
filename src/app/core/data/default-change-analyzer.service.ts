370d428aec693494ee390828e0bd43731ae43906
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2022-05-31 15:27:33 -0400

    Correction du bug #1533 https://github.com/DSpace/dspace-angular/pull/1533/files.

--- src/app/core/data/default-change-analyzer.service.ts
+++ src/app/core/data/default-change-analyzer.service.ts
@@ -3,7 +3,9 @@ import { compare } from 'fast-json-patch';
 import { Operation } from 'fast-json-patch';
 import { getClassForType } from '../cache/builders/build-decorators';
 import { TypedObject } from '../cache/object-cache.reducer';
-import { DSpaceSerializer } from '../dspace-rest/dspace.serializer';
+// UdeM 2022 | correction du bug #1533 https://github.com/DSpace/dspace-angular/pull/1533/files
+// import { DSpaceSerializer } from '../dspace-rest/dspace.serializer';
+import { DSpaceNotNullSerializer  } from '../dspace-rest/dspace-not-null.serializer';
 import { ChangeAnalyzer } from './change-analyzer';
 
 /**
@@ -22,8 +24,8 @@ export class DefaultChangeAnalyzer<T extends TypedObject> implements ChangeAnaly
    *    The second object to compare
    */
   diff(object1: T, object2: T): Operation[] {
-    const serializer1 = new DSpaceSerializer(getClassForType(object1.type));
-    const serializer2 = new DSpaceSerializer(getClassForType(object2.type));
+    const serializer1 = new DSpaceNotNullSerializer(getClassForType(object1.type));
+    const serializer2 = new DSpaceNotNullSerializer(getClassForType(object2.type));
     return compare(serializer1.serialize(object1), serializer2.serialize(object2));
   }
 }
